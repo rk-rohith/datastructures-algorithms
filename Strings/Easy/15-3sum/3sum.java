ad479cd8d4e147a0217749636ccf12c155090069
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2024-08-21 07:21:01 +0530

    Time: 30 ms (67.59%) | Memory: 51.8 MB (45.29%) - LeetSync

--- Strings/Easy/15-3sum/3sum.java
+++ Strings/Easy/15-3sum/3sum.java
@@ -8,13 +8,13 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             //Only consider non-duplicate elements for i
             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int target = 0 - nums[i];
-                //int target = 0;
+                // int target = 0 - nums[i];
+                int target = 0;
                 int left = i + 1;
                 int right = nums.length - 1;
 
                 while (left < right) {
-                    if (nums[left] + nums[right] == target) {
+                    if (nums[i] + nums[left] + nums[right] == target) {
                         sol.add(List.of(nums[i], nums[left], nums[right]));
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
@@ -24,7 +24,7 @@ class Solution {
                         }
                         left++;
                         right--;
-                    } else if (nums[left] + nums[right] > target) {
+                    } else if (nums[i] + nums[left] + nums[right] > target) {
                         right--;
                     } else {
                         left++;
