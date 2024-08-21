c50f7affea7007880fedc98f0b205682d6f337ee
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2024-08-21 07:31:34 +0530

    Time: 34 ms (39.95%) | Memory: 51.5 MB (57.52%) - LeetSync

--- Strings/Easy/15-3sum/3sum.java
+++ Strings/Easy/15-3sum/3sum.java
@@ -1,31 +1,36 @@
 class Solution {
 
-    //2 pointers
+    // 2 pointers
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int target = 0;
         for (int i = 0; i < nums.length - 2; i++) {
-            //Only consider non-duplicate elements for i
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {                
+            // Only consider non-duplicate elements for i
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
                 int left = i + 1;
                 int right = nums.length - 1;
 
                 while (left < right) {
-                    if (nums[i] + nums[left] + nums[right] == target) {                        
-                        result.add(List.of(nums[i], nums[left], nums[right]));
-                        while (left < right && nums[left] == nums[left + 1]) {
-                            left++;
-                        }
-                        while (left < right && nums[right] == nums[right - 1]) {
-                            right--;
-                        }
+                    // Calculate the sum of the triplet and Check if the sum equals the target(zero)
+                    if (nums[i] + nums[left] + nums[right] == target) {
+                        result.add(List.of(nums[i], nums[left], nums[right])); // list for the found triplet and add it to the result
+
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++; // Skip duplicates for the left pointer
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--; // Skip duplicates for the right pointer
+
+                        // Move both pointers to find new pairs
                         left++;
                         right--;
-                    } else if (nums[i] + nums[left] + nums[right] > target) {
-                        right--;
+
+                    } else if (nums[i] + nums[left] + nums[right] < target) {
+                        // If the sum is less than zero, move the left pointer to increase the sum
+                        left++;
                     } else {
-                        left++;
+                        // If the sum is greater than zero, move the right pointer to decrease the sum
+                        right--;
                     }
                 }
             }
