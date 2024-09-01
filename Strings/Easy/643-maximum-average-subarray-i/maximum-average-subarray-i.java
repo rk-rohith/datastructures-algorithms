86ee9984270dc2aec615309061fbecc9a4ba3e98
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2024-09-01 11:14:22 +0530

    Time: 4 ms (68.83%) | Memory: 56.2 MB (45.81%) - LeetSync

--- Strings/Easy/643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ Strings/Easy/643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,33 +1,24 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
 
-        double sum = 0;
-        double maxAvgSum = 0;
+        double sum = 0;               // Initialize sum to store the sum of the first 'k' elements  
+        double maxAvgSum = 0;         // Initialize maxAvgSum to store the maximum average found  
 
+
+        // Calculate the sum of the first 'k' elements  
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
-        maxAvgSum = sum / k;
+        maxAvgSum = sum / k;   // Calculate the initial average from the first 'k' elements  
 
+
+        // Use a sliding window approach to find the maximum average  
         for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];
-            maxAvgSum = Math.max(maxAvgSum, sum / k);
+            sum += nums[i] - nums[i - k];                     // Update the sum by adding the next element and removing the first element of the previous window
+            maxAvgSum = Math.max(maxAvgSum, sum / k);         // Update maxAvgSum if the new average is greater than the previous maximum  
+
         }
         return maxAvgSum;
 
-        // double maxAvg = Integer.MIN_VALUE; // Initialize maxAvg to store the maximum
-        // average found
-
-        // // Iterate through the array to find all possible k-length subarrays
-        // for (int i = 0; i <= nums.length - k; i++) {
-        // double winDowAvg = 0;
-        // // Calculate the sum of the current k-length subarray
-        // for (int j = i; j < i + k; j++) {
-        // winDowAvg += nums[j];
-        // }
-        // // Update maxAvg if the current window's average is greater
-        // maxAvg = Math.max(maxAvg, winDowAvg / k);
-        // }
-        // return maxAvg;
     }
 }
