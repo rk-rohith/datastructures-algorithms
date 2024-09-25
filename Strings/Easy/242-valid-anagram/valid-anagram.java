85c0b059d2b766e756a8977683a97674259fd575
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2024-09-25 22:38:16 +0530

    Time: 4 ms (73.74%) | Memory: 44.5 MB (47.71%) - LeetSync

--- Strings/Easy/242-valid-anagram/valid-anagram.java
+++ Strings/Easy/242-valid-anagram/valid-anagram.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        } else {
-            char[] s1 = s.toLowerCase().toCharArray();
-            char[] t1 = t.toLowerCase().toCharArray();
 
-            Arrays.sort(s1);
-            Arrays.sort(t1);
-            return Arrays.equals(s1,t1);
-        }
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        return Arrays.equals(s1, t1);
 
     }
 }
