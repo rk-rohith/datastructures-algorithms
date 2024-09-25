c1bc8c0304292685a9a0d47a1446cb88616b2828
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2024-09-25 22:32:19 +0530

    Time: 5 ms (24.24%) | Memory: 45.3 MB (46.29%) - LeetSync

--- Strings/Easy/680-valid-palindrome-ii/valid-palindrome-ii.java
+++ Strings/Easy/680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,28 +1,42 @@
-class Solution {
-    public boolean validPalindrome(String s) {
-        if (s.length() == 1)
-            return true;
+class Solution {  
+    // Method to check if a string can be a valid palindrome by removing at most one character  
+    public boolean validPalindrome(String s) {  
+        // If the string length is 1, it is already a valid palindrome  
+        if (s.length() == 1)  
+            return true;  
 
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return isPalindrome(s, left, right - 1) || isPalindrome(s, left + 1, right);
-            } else {
-                left++;
-                right--;
-            }
-        }
-        return true;
-    }
+        // Initialize two pointers, one at the beginning (left) and one at the end (right) of the string  
+        int left = 0;  
+        int right = s.length() - 1;  
+        
+        // Loop until the two pointers meet  
+        while (left < right) {  
+            // If characters at the left and right pointers do not match  
+            if (s.charAt(left) != s.charAt(right)) {  
+                // Check if the string is a palindrome by skipping either the left or the right character  
+                return isPalindrome(s, left, right - 1) || isPalindrome(s, left + 1, right);  
+            } else {  
+                // Move both pointers towards the center if the characters match  
+                left++;  
+                right--;  
+            }  
+        }  
+        // If we finish the loop without finding a mismatch, it is a valid palindrome  
+        return true;  
+    }  
 
-    private static boolean isPalindrome(String str, int i, int j) {
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
+    // Helper method to check if a substring (from index i to j) is a palindrome  
+    private static boolean isPalindrome(String str, int i, int j) {  
+        // Check characters from both ends towards the center  
+        while (i < j) {  
+            // If characters do not match, it is not a palindrome  
+            if (str.charAt(i) != str.charAt(j))  
+                return false;  
+            // Move towards the center  
+            i++;  
+            j--;  
+        }  
+        // If all characters match, it is a palindrome  
+        return true;  
+    }  
 }
