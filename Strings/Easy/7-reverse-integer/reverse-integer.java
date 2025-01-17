4635ccd061c0e0f19be8199ffb33aaaff7588d48
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2025-01-17 08:20:47 +0530

    Time: 1 ms (84.50%) | Memory: 41.6 MB (5.62%) - LeetSync

---
+++ Strings/Easy/7-reverse-integer/reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+
+        int res = 0;
+        while(x !=0)
+        {
+            int tmp = x%10;
+            res = res*10+tmp;
+            x = (x-tmp)/10;
+            
+            if(res%10!=tmp) //exceed int range
+            {
+                return 0;
+            }
+        }
+        
+        return res;
+    }
+}
