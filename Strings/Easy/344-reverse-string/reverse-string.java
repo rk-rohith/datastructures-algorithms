6cc05c4855b7e1efbee241afb58221ddb7028bf0
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2025-03-10 22:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (28.07%) - LeetSync

--- Strings/Easy/344-reverse-string/reverse-string.java
+++ Strings/Easy/344-reverse-string/reverse-string.java
@@ -1,16 +1,21 @@
 class Solution {
     public void reverseString(char[] s) {
+         // Initialize the start pointer to the beginning of the array  
+        int start = 0;  
+        // Initialize the end pointer to the last index of the array  
+        int end = s.length - 1;  
 
-        int left = 0;
-        int right = s.length-1;
-
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        // Continue swapping until the start pointer is less than the end pointer  
+        while (start < end) {  
+            // Temporarily store the character at the start position  
+            char temp = s[start];  
+            // Swap the characters at start and end positions  
+            s[start] = s[end];  
+            s[end] = temp;  
+            // Move the start pointer forward  
+            start++;  
+            // Move the end pointer backward  
+            end--;  
         }
-        
     }
 }
